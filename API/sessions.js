3eb14fd2edbaacd9dbb54cf0700cea0ff8a895a5
Muneeb <muneebiftikhar382@gmail.com>
2021-09-02 18:51:22 +0500

    user_login Fix

--- API/sessions.js
+++ API/sessions.js
@@ -4,13 +4,13 @@ const router = express.Router();
 const User = require("../Modules/user");
 
 router.post("/sessions", async (req, res) => {
-    User.find({email: req.body.email, password: req.body.password})
+    User.findOne({email: req.body.email, password: req.body.password})
         .exec()
         .then(user => {
-            if(user.length >= 1) {
-                return res.status(409).json({user});
-            };
-        })
+                 res.status(201).json({
+                     user
+             })
+    })
         .catch((err) => {
         res.status(401).json({
             error: err
@@ -21,24 +21,3 @@ router.post("/sessions", async (req, res) => {
 
 
 module.exports = router;
-
-
-// await User.find(req.body.email)
-//     .exec()
-//     .then(result => {
-//         console.log(result);
-//         if(!result) {
-//             return res.status(404).json({
-//               message: "Email not found"
-//             })
-//           }
-//         res.status(200).json({
-//             _id: result._id,
-//             name: result.name,
-//         });
-//     })
-//     .catch(err => {
-//         res.status(500).json({
-//             error: err
-//         })
-//     });
